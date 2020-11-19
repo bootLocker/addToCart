8fe2dc4f70ce4ce02373bb4cd99f0acb265581d2
Timothy Hansen <timothyhansen229@gmail.com>
2020-11-19 10:41:55 -0800

    Css complete. Put css file in src folder as a backup to commit.

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -34,50 +34,53 @@ class Home extends React.Component {
     console.log(this.state.shoeData[3]);
     if (this.state.shoeData && this.state.shoeData.length > 0) {
       return (
-        <div name='mainDiv'>
+        <div id='headDiv'>
           <div>
             <span className="home"> Home </span> &nbsp;
             <span className="currently">  > {this.state.shoeData[3].shoeName + " - " + this.state.shoeData[3].gender} </span>
           </div>
 
-          <div id='mainComponentDiv'>
+          <div className='mainComponentDiv'>
+
             <div id='leftPortion'>
               <div className="mainShoeDiv">
                 {/* using other color ways image because it is bigger/better qual */}
                 <div className='mainShoeImg'>
-                  <img src={this.state.shoeData[3].otherColorWays[0]} />
+                  <img src='https://images.footlocker.com/is/image/EBFL2/FU7385_a1?wid=630&hei=630&fmt=png-alpha' />
+                  {/* <img src={this.state.shoeData[3].otherColorWays[0]} /> */}
                 </div>
               </div>
               <div className="mainShoeAlternateImgs">
-                <ul>
-                  {this.state.shoeData[3].currentShoePictures.map((shoe, index) => {
-                    return (
-                      <div className='indivAltMainShoeImg'>
-                        <img src={shoe} />
-                      </div>
-                    );
-                  })}
-                </ul>
+                {this.state.shoeData[3].currentShoePictures.map((shoe, index) => {
+                  return (
+                    <div className='indivAltMainShoeImg'>
+                      <img src={shoe} />
+                    </div>
+                  );
+                })}
               </div>
             </div>
 
             <div id='rightPortion'>
               <div className="shoeName">
-                <h1> {this.state.shoeData[3].shoeName} </h1>
+                <h1 > {this.state.shoeData[3].shoeName} </h1>
               </div>
               <div id="gender">
                 <span> {this.state.shoeData[3].gender} </span>
               </div>
+
               <div id="ratings">
-                <span> {'(' + this.state.shoeData[3].rating + ')'}</span>
-                <a className="reviewAndInfo" href=""> Write a review </a>
+                <span className='starz'> ☆☆☆☆☆ </span>
+                &nbsp;<span className='ratingPercentage'> 4.0 </span>&nbsp;
+                <span> {'(' + this.state.shoeData[3].rating + ')'}</span> &nbsp;&nbsp;&nbsp;&nbsp;
+                <a className="reviewAndInfo" href="">Write a review </a> &nbsp;&nbsp;&nbsp;&nbsp;
                 <a className="reviewAndInfo" href=""> Request info </a>
               </div>
               <div id="otherColorwaysImgs">
                 {this.state.shoeData[3].otherColorWays.map((shoe, index) => {
                   return (
-                    <div className="otherShoes">
-                      <img src={shoe} />
+                    <div className="otherShoesBackground">
+                      <img className='otherShoesImage' src={shoe} />
                     </div>
                   );
                 })}
@@ -99,13 +102,13 @@ class Home extends React.Component {
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a className='sizeChart' href="">Size Chart </a>
               </div>
               <div className='makePayments'>
                 <span> 4 interest-free payments of ${Number.parseInt(this.state.shoeData[3].price.slice(1) / 4, 10)}. <b>Klarna.</b> </span>
                 <br />
-                <a className='learnMore'> Learn more </a>
+                <span className='learnMorePayments'> Learn more </span>
               </div>
 
               <div id="shoeSizes">
@@ -122,8 +125,13 @@ class Home extends React.Component {
                 <div className='flxLogo'>
                   <img src="https://www.footlocker.com/content/dam/flincfoundation/loyalty/flx_logo.svg" />
                 </div>
-                <span className='freeShipping'> <b> MEMBERS GET FREE SHIPPING  </b> </span> <br />
-                <span className='learnMore'> Learn more </span>
+                <div className='learnShipping'>
+                  <span className='freeShipping'> <b> MEMBERS GET FREE SHIPPING  </b> </span>   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className='learnMore'> Learn more </span>
+                </div>
               </div>
 
               <div>
